81f319547eb94bdef75456c788a79340c60b4577
Hannu76 <156414707+Hannu76@users.noreply.github.com>
2026-07-22 16:05:34 +0530

    Add files via upload

---
+++ src/utils/images.js
@@ -0,0 +1,51 @@
+import bills from '../assets/images/bills.png';
+import chatgpt_round from '../assets/images/chatgpt_round.png';
+import claude_round from '../assets/images/claude_round.png';
+import commute from '../assets/images/commute.png';
+import currency from '../assets/images/currency.png';
+import currency_conversion from '../assets/images/currency_conversion.png';
+import education from '../assets/images/education.png';
+import emi from '../assets/images/emi.png';
+import entertainment from '../assets/images/entertainment.png';
+import favicon from '../assets/images/favicon.png';
+import flywire from '../assets/images/flywire.jpg';
+import food from '../assets/images/food.png';
+import gemini_round from '../assets/images/gemini_round.png';
+import healthcare from '../assets/images/healthcare.png';
+import insurance from '../assets/images/insurance.png';
+import loan_balance from '../assets/images/loan_balance.png';
+import misc from '../assets/images/misc.png';
+import poll from '../assets/images/poll.png';
+import prepayment_target from '../assets/images/prepayment_target.png';
+import rent from '../assets/images/rent.png';
+import save from '../assets/images/save.png';
+import shopping from '../assets/images/shopping.png';
+import target from '../assets/images/target.png';
+
+export const IMAGES = {
+  bills,
+  chatgpt_round,
+  claude_round,
+  commute,
+  currency,
+  currency_conversion,
+  education,
+  emi,
+  entertainment,
+  favicon,
+  flywire,
+  food,
+  gemini_round,
+  healthcare,
+  insurance,
+  loan_balance,
+  misc,
+  poll,
+  prepayment_target,
+  rent,
+  save,
+  shopping,
+  target,
+};
+
+export default IMAGES;
