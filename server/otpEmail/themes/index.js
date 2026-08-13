31e873c76ec764cb973ea10061476d84ed5d8a24
Hannu76 <156414707+Hannu76@users.noreply.github.com>
2026-08-13 15:32:38 +0530

    Add files via upload

---
+++ server/otpEmail/themes/index.js
@@ -0,0 +1,100 @@
+const themes = [
+    {
+        name: 'Electric Purple',
+        primary: '#5B21FF',
+        primaryDark: '#4414D2',
+        light: '#F3EFFF',
+        soft: '#F8F5FF',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Bright Blue',
+        primary: '#146EF5',
+        primaryDark: '#0954C7',
+        light: '#EAF3FF',
+        soft: '#F5F9FF',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Cyan',
+        primary: '#00AEEF',
+        primaryDark: '#008CBE',
+        light: '#E5F7FF',
+        soft: '#F2FBFF',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Electric Pink',
+        primary: '#FF1493',
+        primaryDark: '#D90073',
+        light: '#FFF0F7',
+        soft: '#FFF8FB',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Hot Magenta',
+        primary: '#E600FF',
+        primaryDark: '#B300C7',
+        light: '#FDE5FF',
+        soft: '#FEF5FF',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Bright Red',
+        primary: '#FF1744',
+        primaryDark: '#D5002C',
+        light: '#FFE8EC',
+        soft: '#FFF5F6',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Vivid Orange',
+        primary: '#FF6D00',
+        primaryDark: '#E85C00',
+        light: '#FFF3E8',
+        soft: '#FFF9F4',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Bright Amber',
+        primary: '#FFB300',
+        primaryDark: '#D99600',
+        light: '#FFF9E6',
+        soft: '#FFFCF3',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Lime',
+        primary: '#76FF03',
+        primaryDark: '#5BD600',
+        light: '#EDFFDE',
+        soft: '#F6FFED',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Emerald',
+        primary: '#00C853',
+        primaryDark: '#00973F',
+        light: '#E6F9EC',
+        soft: '#F3FCF6',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Turquoise',
+        primary: '#00E5FF',
+        primaryDark: '#00B8CC',
+        light: '#E6FCFF',
+        soft: '#F3FEFF',
+        contrast: '#FFFFFF'
+    },
+    {
+        name: 'Royal Violet',
+        primary: '#7C00FF',
+        primaryDark: '#5E00CC',
+        light: '#F3E5FF',
+        soft: '#F8F2FF',
+        contrast: '#FFFFFF'
+    }
+];
+
+export default themes;
