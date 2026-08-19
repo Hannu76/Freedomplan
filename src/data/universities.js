1d06d51d429cab51c6f770044f381c917a21e186
Hannu76 <156414707+Hannu76@users.noreply.github.com>
2026-08-19 18:59:16 +0530

    Add files via upload

---
+++ src/data/universities.js
@@ -0,0 +1,135 @@
+/**
+ * Official UK University Brand Assets & Verified Domain dataset for the "Trusted by Universities" Marquee.
+ * All paths map directly to official PNG assets in /public/universities/
+ */
+
+export const UNIVERSITIES_DATA = [
+  {
+    id: 'coventry',
+    name: 'Coventry University',
+    shortName: 'Coventry',
+    logo: '/universities/coventry-university.png',
+    url: 'https://www.coventry.ac.uk/',
+  },
+  {
+    id: 'york',
+    name: 'University of York',
+    shortName: 'York',
+    logo: '/universities/university-of-york.png',
+    url: 'https://www.york.ac.uk/',
+  },
+  {
+    id: 'teesside',
+    name: 'Teesside University',
+    shortName: 'Teesside',
+    logo: '/universities/teesside-university.png',
+    url: 'https://www.tees.ac.uk/',
+  },
+  {
+    id: 'hertfordshire',
+    name: 'University of Hertfordshire',
+    shortName: 'Hertfordshire',
+    logo: '/universities/university-of-hertfordshire.png',
+    url: 'https://www.herts.ac.uk/',
+  },
+  {
+    id: 'uel',
+    name: 'University of East London',
+    shortName: 'East London',
+    logo: '/universities/university-of-east-london.png',
+    url: 'https://www.uel.ac.uk/',
+  },
+  {
+    id: 'roehampton',
+    name: 'University of Roehampton London',
+    shortName: 'Roehampton',
+    logo: '/universities/university-of-roehampton.png',
+    url: 'https://www.roehampton.ac.uk/',
+  },
+  {
+    id: 'bpp',
+    name: 'BPP University',
+    shortName: 'BPP',
+    logo: '/universities/bpp-university.png',
+    url: 'https://www.bpp.com/',
+  },
+  {
+    id: 'ulster',
+    name: 'Ulster University',
+    shortName: 'Ulster',
+    logo: '/universities/ulster-university.png',
+    url: 'https://www.ulster.ac.uk/',
+  },
+  {
+    id: 'sheffield',
+    name: 'University of Sheffield',
+    shortName: 'Sheffield',
+    logo: '/universities/university-of-sheffield.png',
+    url: 'https://www.sheffield.ac.uk/',
+  },
+  {
+    id: 'manchester',
+    name: 'The University of Manchester',
+    shortName: 'Manchester',
+    logo: '/universities/manchester-university.png',
+    url: 'https://www.manchester.ac.uk/',
+  },
+  {
+    id: 'leeds',
+    name: 'University of Leeds',
+    shortName: 'Leeds',
+    logo: '/universities/university-of-leeds.png',
+    url: 'https://www.leeds.ac.uk/',
+  },
+  {
+    id: 'greenwich',
+    name: 'University of Greenwich',
+    shortName: 'Greenwich',
+    logo: '/universities/university-of-greenwich.png',
+    url: 'https://www.gre.ac.uk/',
+  },
+  {
+    id: 'york-st-john',
+    name: 'York St John University',
+    shortName: 'York St John',
+    logo: '/universities/york-st-john-university.png',
+    url: 'https://www.yorksj.ac.uk/',
+  },
+  {
+    id: 'cambridge',
+    name: 'University of Cambridge',
+    shortName: 'Cambridge',
+    logo: '/universities/university-of-cambridge.png',
+    url: 'https://www.cam.ac.uk/',
+  },
+  {
+    id: 'oxford',
+    name: 'University of Oxford',
+    shortName: 'Oxford',
+    logo: '/universities/university-of-oxford.png',
+    url: 'https://www.ox.ac.uk/',
+  },
+  {
+    id: 'london',
+    name: 'University of London',
+    shortName: 'London',
+    logo: '/universities/university-of-london.png',
+    url: 'https://www.london.ac.uk/',
+  },
+  {
+    id: 'birmingham',
+    name: 'University of Birmingham',
+    shortName: 'Birmingham',
+    logo: '/universities/university-of-birmingham.png',
+    url: 'https://www.birmingham.ac.uk/',
+  },
+  {
+    id: 'warwick',
+    name: 'University of Warwick',
+    shortName: 'Warwick',
+    logo: '/universities/university-of-warwick.png',
+    url: 'https://warwick.ac.uk/',
+  }
+];
+
+export default UNIVERSITIES_DATA;
